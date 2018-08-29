c5b1c56af6ac1f3f2636ef9d5edaba521d0a443f
Dalton Lima <daltonvarussa@gmail.com>
2018-08-29 09:27:00 -0300

    add AtlasManager

---
+++ Assets/Scripts/AtlasManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AtlasManager : MonoBehaviour, IManager
+{
+    public static Sprite[] sprites;
+    public ManagerState CurrentState { get; private set; }
+
+    public void BootSequence()
+    {
+        Debug.Log(string.Format("{0} is booting up", GetType().Name));
+
+        sprites = Resources.LoadAll<Sprite>("EventAtlas");
+        CurrentState = ManagerState.Completed;
+
+        Debug.Log(string.Format("{0} status = {1] ", GetType().Name, CurrentState));
+    }
+
+    public static Sprite LoadSprite(string spriteName)
+    {
+        foreach (Sprite s in sprites)
+        {
+            if (s.name == spriteName) { return s; }
+        }
+
+        return null;
+    }
+}
