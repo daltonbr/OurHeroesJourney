bdd3dce259cbb5ce8c7f3d928f2e78f9c4156230
Dalton Lima <daltonvarussa@gmail.com>
2018-08-29 11:44:47 -0300

    add  json Dialogue (Event1) and a class to represent it (NarrativeEvent.cs)

---
+++ Assets/Scripts/NarrativeEvent.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NarrativeEvent
+{
+    public List<Dialogue> dialogues;
+}
+
+public struct Dialogue
+{
+    public CharacterType characterType;
+    public string name;
+    public string atlasImageName;
+    public string dialogueText;
+}
+
+public enum CharacterType
+{
+    Hero, Ally, Mentor
+}
