18683bc3e879ebb68e889f161d9bf4bb4dc2a5f0
Dalton Lima <daltonvarussa@gmail.com>
2018-08-29 23:58:51 -0300

    add player input

--- Assets/Scripts/MasterManager.cs
+++ Assets/Scripts/MasterManager.cs
@@ -4,20 +4,24 @@ using UnityEngine;
 
 [RequireComponent(typeof(AtlasManager))]
 [RequireComponent(typeof(AnimationManager))]
+[RequireComponent(typeof(PanelManager))]
 public class MasterManager : MonoBehaviour
 {
     private List<IManager> _managerList = new List<IManager>();
 
     public static AtlasManager AtlasManager { get; private set; }
     public static AnimationManager AnimationManager { get; private set; }
+    public static PanelManager PanelManager { get; private set; }
 
     private void Awake()
     {
         AtlasManager = GetComponent<AtlasManager>();
         AnimationManager = GetComponent<AnimationManager>();
+        PanelManager = GetComponent<PanelManager>();
 
         _managerList.Add(AtlasManager);
         _managerList.Add(AnimationManager);
+        _managerList.Add(PanelManager);
 
         StartCoroutine(BootAllManagers());
     }
---
+++ Assets/Scripts/PanelManager.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using JSONFactory;
+
+public class PanelManager : MonoBehaviour, IManager
+{
+    public ManagerState CurrentState { get; private set; }
+
+    private PanelConfig _rightPanel;
+    private PanelConfig _leftPanel;
+
+    private NarrativeEvent _currentEvent;
+    private bool _leftCharacterActive = true;
+    private int stepIndex = 0;
+
+    public void BootSequence()
+    {
+        Debug.Log(string.Format("{0} is booting up", GetType().Name));
+
+        // TODO: make a better way to get these references
+        _rightPanel = GameObject.Find("RightCharacterPanel").GetComponent<PanelConfig>();
+        _leftPanel = GameObject.Find("LeftCharacterPanel").GetComponent<PanelConfig>();
+        _currentEvent = JSONAssembly.RunJSONFactoryForScene(1);
+        InitializePanels();
+
+        Debug.Log(string.Format("{0} status = {1}", GetType().Name, CurrentState));
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            UpdatePanelState();
+        }
+    }
+
+
+    private void InitializePanels()
+    {
+        _leftPanel.CharacterIsTalking = true;
+        _rightPanel.CharacterIsTalking = false;
+        _leftCharacterActive = !_leftCharacterActive;
+
+        _leftPanel.Configure(_currentEvent.dialogues[stepIndex]);
+        _rightPanel.Configure(_currentEvent.dialogues[stepIndex + 1]);
+
+        StartCoroutine(MasterManager.AnimationManager.IntroAnimation());
+
+        stepIndex++;
+    }
+
+    private void ConfigurePanels()
+    {
+        if (_leftCharacterActive)
+        {
+            _leftPanel.CharacterIsTalking = true;
+            _rightPanel.CharacterIsTalking = false;
+
+            _leftPanel.Configure(_currentEvent.dialogues[stepIndex]);
+            _rightPanel.ToggleCharacterMask();
+        }
+        else
+        {
+            _leftPanel.CharacterIsTalking = false;
+            _rightPanel.CharacterIsTalking = true;
+
+            _leftPanel.ToggleCharacterMask();
+            _rightPanel.Configure(_currentEvent.dialogues[stepIndex]);
+        }
+    }
+    private void UpdatePanelState()
+    {
+        if (stepIndex < _currentEvent.dialogues.Count)
+        {
+            ConfigurePanels();
+
+            _leftCharacterActive = !_leftCharacterActive;
+            stepIndex++;
+        }
+        else
+        {
+            StartCoroutine(MasterManager.AnimationManager.ExitAnimation());
+        }
+    }
+
+}
